a3a4836f80c56a9de00234173b94bf20959388c2
sivavithu <sivavithu15@live.com>
2025-10-02 14:37:05 +0530

    removal odl

--- AuthService/Models/TokenResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuthService.Models
-{
-    public class TokenResponseDto
-    {
-        public string AccessToken { get; set; } = string.Empty;
-        public string RefreshToken { get; set; } = string.Empty;
-    }
-}
